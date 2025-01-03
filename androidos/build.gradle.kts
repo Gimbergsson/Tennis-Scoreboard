f4487bdfe5a1041b5b906b2fc28f588533f2bb4f
Dennis Gimbergsson <dennisgimbergsson@live.se>
2025-01-03 20:29:25 +0100

    fix: use generated version name for artifact

--- androidos/build.gradle.kts
+++ androidos/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "se.dennisgimbergsson.tennisscoreboard"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -45,12 +45,14 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("releaseConfig")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("releaseConfig")
         }
     }
 
