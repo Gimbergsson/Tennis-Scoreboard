1acde018fe9238c73907c513d1d867af4d702cc7
Dennis Gimbergsson <dennisgimbergsson@live.se>
2025-01-03 14:13:53 +0100

    chore: Add patch number to version name

--- androidos/build.gradle.kts
+++ androidos/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
