163886dba3d5b1a50e1a4133c832e0a454b7bd0b
Dennis Gimbergsson <dennisgimbergsson@live.se>
2025-01-08 20:28:01 +0100

    feat: Build and upload both Android OS and Wear OS apps

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -7,6 +10,19 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+val apikeyPropertiesFile = rootProject.file("apikey.properties")
+val apikeyProperties = Properties()
+apikeyProperties.load(FileInputStream(apikeyPropertiesFile))
+
+@Suppress("UnstableApiUsage")
+val gitBranchName = providers.exec {
+    commandLine("git", "rev-parse", "--abbrev-ref", "HEAD")
+}.standardOutput.asText.get()
+
 android {
     namespace = "se.dennisgimbergsson.tennisscoreboard"
     compileSdk = 35
@@ -14,16 +30,28 @@ android {
     defaultConfig {
         applicationId = "se.dennisgimbergsson.tennisscoreboard"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "${buildVersionName()} ($versionCode)"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("releaseConfig") {
+            keyAlias = keystoreProperties["KEYSTORE_KEY_ALIAS"] as String?
+            keyPassword = keystoreProperties["KEYSTORE_KEY_PASSWORD"] as String?
+            storeFile = file(keystoreProperties["KEYSTORE_FILE_PATH"] as String)
+            storePassword = keystoreProperties["KEYSTORE_PASSWORD"] as String
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("releaseConfig")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -48,6 +76,7 @@ android {
     }
 
     kotlinOptions {
+        languageVersion = "2.0"
         jvmTarget = "17"
     }
 }
@@ -122,4 +151,38 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.unit)
     // Needed for createComposeRule(), but not for createAndroidComposeRule<YourActivity>():
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+tasks.register("printBuildName") {
+    val versionName = android.defaultConfig.versionName?.replace(".", "_")
+        ?.replace(" ", "-#")
+        ?.replace("(", "")
+        ?.replace(")", "")
+    doLast {
+        println("$versionName")
+    }
+}
+
+private fun buildVersionName(): String {
+    var versionName = "undefined-version-name"
+    try {
+        // Gets the full branch name from Git.
+        val branchName = gitBranchName.trim()
+
+        // Only take the identifier part from the full branch name
+        // example from "release/1-0-0" to "1-0-0".
+        val branchIdentifierName = gitBranchName.trim()
+            .split("/")
+            .last()
+
+        // If it's a release build then replace the strokes with dots.
+        versionName = if (branchName.startsWith("release")) {
+            branchIdentifierName.replace("-", ".")
+        } else {
+            branchIdentifierName
+        }
+    } catch (ignored: Exception) {
+        println("Failed to get git branch")
+    }
+    return versionName
 }
