7d1d694f2e5fd9017b7411d9fadd177553e189b1
Dennis Gimbergsson <dennisgimbergsson@live.se>
2025-04-14 23:11:23 +0200

    chore: upgrade wearos target sdk

--- wearos/build.gradle.kts
+++ wearos/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     defaultConfig {
         applicationId = "se.dennisgimbergsson.tennisscoreboard"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "${buildVersionName()} ($versionCode)"
         vectorDrawables.useSupportLibrary = true
